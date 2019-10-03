5558eab1933618d988c19a03d0e0ce63989b389c
STBN <25061425+stbn200@users.noreply.github.com>
2019-10-03 11:15:24 -0500

    https://www.udemy.com/course/react-js-redux-es6-completo-de-0-a-experto-espanol/learn/lecture/8946524#overview
    https://www.udemy.com/course/react-js-redux-es6-completo-de-0-a-experto-espanol/learn/lecture/8946524#overview

---
+++ WeatherApp/weather-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// start
+import WeatherLocation from './components/WeatherLocation'
+// end
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherLocation/>
+    </div>
+  );
+}
+
+export default App;
