22d00604ee1a7c6ae3ef94523e50883a61a052c4
STBN <25061425+stbn200@users.noreply.github.com>
2019-10-23 11:03:46 -0500

    Update index.js

--- WeatherApp/weather-app/src/components/WeatherLocation/index.js
+++ WeatherApp/weather-app/src/components/WeatherLocation/index.js
@@ -4,6 +4,11 @@ import WeatherData from './WeatherData'
 import './styles.css';
 import { SUN } from './../../constants/weathers';
 
+const location = 'Buenos Aires,ar';
+const api_key = '8d6bf15419511b237e8d1cfc2ea85ad1';
+const url_base_weather = 'http://api.openweathermap.org/data/2.5/weather';
+const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`;
+
 const data = {
     temperature: 2001,
     weatherState: 'rain',
@@ -28,9 +33,37 @@ class WeatherLocation extends Component {
             data: dataB,
         };
     }
+    getWeatherState = weather_data => {
+        return 'SUN'
+    }
 
+    getData = weather_data => {
+        const { humidity, temp } = weather_data.main;
+        const { speed } = weather_data.wind;
+        const weatherState = this.getWeatherState(weather_data);
+
+        const data = {
+            humidity,
+            temperature: temp,
+            weatherState,
+            wind: `${speed} m/s`,
+        }
+        return data;
+    }
 
     handleUpdateClick = () => {
+
+        fetch(api_weather).then(resolve => {
+            return resolve.json();
+        }).then(data => {
+            const newWeather = this.getData(data);
+            this.setState({
+                data: newWeather
+            })
+            //console.log(data);
+            debugger;
+        });
+
         this.setState(({
             city: 'Barcelona',
             data: data
