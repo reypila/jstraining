c69a4f17d083ec7941c0cb910e7d35dacf805e91
STBN <25061425+stbn200@users.noreply.github.com>
2019-11-14 14:13:20 -0600

    555
    5555

--- WeatherApp/weather-app/src/App.js
+++ WeatherApp/weather-app/src/App.js
@@ -2,10 +2,20 @@ import React from 'react';
 import LocationList from './components/LocationList';
 import './App.css';
 
+const cities = [
+  'Buenos Aires,ar',
+  'Washington,us',
+  'Bogota,col',
+  'Ciudad de Mexico,mx',
+  'Madrid,es',
+  'Lima,pe'
+
+];
+
 function App() {
   return (
     <div className="App">
-      <LocationList></LocationList>
+      <LocationList cities={cities}></LocationList>
     </div>
   );
 }
--- WeatherApp/weather-app/src/components/LocationList.js
+++ WeatherApp/weather-app/src/components/LocationList.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
 import WeatherLocation from './WeatherLocation/index';
+import PropTypes from 'prop-types';
 
-const LocationList = () => (
+const LocationList = ({ cities }) => {
+    console.log(cities);
+    return (
     <div>
         <WeatherLocation city='Buenos Aires,ar'></WeatherLocation>
-        <WeatherLocation city='washington,us'></WeatherLocation>
+        <WeatherLocation city='Bogota,col'></WeatherLocation>
+        <WeatherLocation city='Washington,us'></WeatherLocation>
+        <WeatherLocation city='Madrid,es'></WeatherLocation>
         <WeatherLocation city='Mexico,mx'></WeatherLocation>
     </div>
-);
+    )
+};
+
+LocationList.propTypes = {
+    cities: PropTypes.array.isRequired,
+};
 
 export default LocationList;
