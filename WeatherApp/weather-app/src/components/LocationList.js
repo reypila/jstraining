ed6339bba0fba9b28f079f06467a42ceeccee3fe
STBN <25061425+stbn200@users.noreply.github.com>
2019-11-22 13:39:21 -0600

    Update LocationList.js

--- WeatherApp/weather-app/src/components/LocationList.js
+++ WeatherApp/weather-app/src/components/LocationList.js
@@ -5,13 +5,13 @@ import PropTypes from 'prop-types';
 const LocationList = ({ cities }) => {
     console.log(cities);
     return (
-    <div>
-        <WeatherLocation city='Buenos Aires,ar'></WeatherLocation>
-        <WeatherLocation city='Bogota,col'></WeatherLocation>
-        <WeatherLocation city='Washington,us'></WeatherLocation>
-        <WeatherLocation city='Madrid,es'></WeatherLocation>
-        <WeatherLocation city='Mexico,mx'></WeatherLocation>
-    </div>
+        <div>
+            <WeatherLocation city='Buenos Aires,ar'></WeatherLocation>
+            <WeatherLocation city='Bogota,col'></WeatherLocation>
+            <WeatherLocation city='Washington,us'></WeatherLocation>
+            <WeatherLocation city='Madrid,es'></WeatherLocation>
+            <WeatherLocation city='Mexico,mx'></WeatherLocation>
+        </div>
     )
 };
 
