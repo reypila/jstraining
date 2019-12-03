1e98fe5c115e20431ec37b0674af393ef2dcb3b8
STBN <25061425+stbn200@users.noreply.github.com>
2019-12-03 11:46:42 -0600

    20191203.1146.73

--- WeatherApp/weather-app/src/App.js
+++ WeatherApp/weather-app/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import LocationList from './components/LocationList';
 import './App.css';
-import { MuiThemeProvider } from '@material-ui/core';
+import { MuiThemeProvider, } from '@material-ui/core';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 
 const cities = [
   'Buenos Aires,ar',
@@ -13,21 +14,48 @@ const cities = [
 ];
 
 class App extends Component {
-  
+
   handleSelection = city => {
     console.log(`handleSelectionLocation ${city}`);
   }
 
   render() {
     return (
+
       <MuiThemeProvider>
-
-        <div className="App">
-          <LocationList cities={cities}
-            onSelectedLocation={this.handleSelection}></LocationList>
-        </div>
-
+        <Grid fluid>
+          <Row>
+            <h1>Grid System: xs ,sm ,md, lg</h1>
+          </Row>
+          <Row>
+            <h5>Las columnas se muestran en la misma fila mientras sumen 12</h5>
+          </Row>
+          <Row>
+            <Col xs={12} sm={6} md={4} lg={3}>
+              <div className='red'></div>
+            </Col>
+            <Col xs={12} sm={6} md={4} lg={3}>
+              <div className='green'></div>
+            </Col>
+            <Col xs={12} sm={6} md={4} lg={3}>
+              <div className='blue'></div>
+            </Col>
+            <Col xs={12} sm={6} md={4} lg={3}>
+              <div className='yellow'></div>
+            </Col>
+          </Row>
+        </Grid>
       </MuiThemeProvider>
+
+
+      // <MuiThemeProvider>
+
+      //   <div className="App">
+      //     <LocationList cities={cities}
+      //       onSelectedLocation={this.handleSelection}></LocationList>
+      //   </div>
+
+      // </MuiThemeProvider>
     );
   }
 }
