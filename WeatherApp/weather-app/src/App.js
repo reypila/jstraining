a0433122ea2cbbadd6caf018874cfc644938a170
STBN <25061425+stbn200@users.noreply.github.com>
2019-12-04 12:21:12 -0600

    Update App.js

--- WeatherApp/weather-app/src/App.js
+++ WeatherApp/weather-app/src/App.js
@@ -22,40 +22,53 @@ class App extends Component {
   render() {
     return (
 
-      <MuiThemeProvider>
-        <Grid fluid>
-          <Row>
-            <h1>Grid System: xs ,sm ,md, lg</h1>
-          </Row>
-          <Row>
-            <h5>Las columnas se muestran en la misma fila mientras sumen 12</h5>
-          </Row>
-          <Row>
-            <Col xs={12} sm={6} md={4} lg={3}>
-              <div className='red'></div>
-            </Col>
-            <Col xs={12} sm={6} md={4} lg={3}>
-              <div className='green'></div>
-            </Col>
-            <Col xs={12} sm={6} md={4} lg={3}>
-              <div className='blue'></div>
-            </Col>
-            <Col xs={12} sm={6} md={4} lg={3}>
-              <div className='yellow'></div>
-            </Col>
-          </Row>
-        </Grid>
-      </MuiThemeProvider>
-
-
       // <MuiThemeProvider>
-
-      //   <div className="App">
-      //     <LocationList cities={cities}
-      //       onSelectedLocation={this.handleSelection}></LocationList>
-      //   </div>
-
+      //   <Grid fluid>
+      //     <Row>
+      //       <h1>Grid System: xs ,sm ,md, lg</h1>
+      //     </Row>
+      //     <Row>
+      //       <h5>Las columnas se muestran en la misma fila mientras sumen 12</h5>
+      //     </Row>
+      //     <Row>
+      //       <Col xs={12} sm={6} md={4} lg={3}>
+      //         <div className='red'></div>
+      //       </Col>
+      //       <Col xs={12} sm={6} md={4} lg={3}>
+      //         <div className='green'></div>
+      //       </Col>
+      //       <Col xs={12} sm={6} md={4} lg={3}>
+      //         <div className='blue'></div>
+      //       </Col>
+      //       <Col xs={12} sm={6} md={4} lg={3}>
+      //         <div className='yellow'></div>
+      //       </Col>
+      //     </Row>
+      //   </Grid>
       // </MuiThemeProvider>
+
+      <Grid>
+        <Row>
+          Titulo
+        </Row>
+        <Row>
+          <Col xs={12} md={6}>
+            <LocationList
+              cities={cities}
+              onSelectedLocation={this.handleSelectionLocation}>
+            </LocationList>
+          </Col>
+          <Col xs={12} md={6}>
+            <div className='details'></div>
+          </Col>
+        </Row>
+      </Grid>
+
+      // <div className="App">
+      //   <LocationList cities={cities}
+      //     onSelectedLocation={this.handleSelection}></LocationList>
+      // </div>
+      
     );
   }
 }
